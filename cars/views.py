fb4f5d384dfb85f602bd4f0f317b1f30d1b16293
Francisco <franciscojdsjr@gmail.com>
2023-10-03 06:26:05 -0300

    att

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def cars_view(request):
+    
+    html = '''
+        <html>
+        <head>
+            <title> Meus Carros </title>
+        </head>
+        <body>
+            <h1> Carros da PycodeBR </h1>
+            <h3> Só carro top </h3>
+        </body>    
+        </html>
+    '''
+    
+    return HttpResponse(html)
