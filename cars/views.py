24455483efb9ba56ed7b82e9d3487989b18d2665
Francisco <franciscojdsjr@gmail.com>
2023-10-07 07:49:46 -0300

    att

--- cars/views.py
+++ cars/views.py
@@ -1,18 +1,11 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from cars.models import Car
 
 
 # Create your views here.
 
 def cars_view(request):
-    html = '''
-        <html>
-        <head>
-            <title> Meus Carros </title>
-        </head>
-        <body>
-            <h1> Carros da PycodeBR </h1>
-            <h3> Só carro top </h3>
-        </body>
-        </html>
-    '''
-    return HttpResponse(html)
+    cars = Car.objects.all()
+    return render(request=request,
+                  template_name='cars.html',
+                  context={'cars': cars})
