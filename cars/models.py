c398cba7d49b31db97abdcaec9e472da66d14748
Francisco <franciscojdsjr@gmail.com>
2023-10-08 06:49:07 -0300

    ATT

--- cars/models.py
+++ cars/models.py
@@ -2,12 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+
 class Brand(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200, blank=False, null= False)
-    
+
     def __str__(self):
         return self.name
+
 
 class Car(models.Model):
     id = models.AutoField(primary_key=True)
@@ -18,9 +20,6 @@ class Car(models.Model):
     plate = models.CharField(max_length=10, blank=True, null=True)
     value = models.FloatField(blank=True, null=True)
     photo = models.ImageField(upload_to='cars/', blank=True, null=True)
-    
+
     def __str__(self):
         return self.model
-
-    
-    
--- cars/views.py
+++ cars/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from cars.models import Car
 
-
 # Create your views here.
+# Order_by com - no inicio ordena decrecente (-model)
+
 
 def cars_view(request):
-    cars = Car.objects.all()
+    cars = Car.objects.all().order_by('-model')
     search = request.GET.get('search')
+    print(search)
     if search:
-        cars = Car.filter(model__contains=search)
+        cars = Car.objects.filter(model__icontains=search)
     return render(request=request,
                   template_name='cars.html',
                   context={'cars': cars})
