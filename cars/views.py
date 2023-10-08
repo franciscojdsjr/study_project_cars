84e3aec46d3210b10c562bdb310896aaf97964eb
Francisco <franciscojdsjr@gmail.com>
2023-10-08 05:50:19 -0300

    att

--- cars/views.py
+++ cars/views.py
@@ -6,6 +6,9 @@ from cars.models import Car
 
 def cars_view(request):
     cars = Car.objects.all()
+    search = request.GET.get('search')
+    if search:
+        cars = Car.filter(model__contains=search)
     return render(request=request,
                   template_name='cars.html',
                   context={'cars': cars})
