e750b8eff142aa0367a28d6532de25ba7f56b563
Francisco <franciscojdsjr@gmail.com>
2023-11-08 07:46:49 -0300

    att

--- cars/signals.py
+++ cars/signals.py
@@ -1,1 +1,23 @@
 from django.db.models.signals import pre_delete, pre_save, post_save, post_delete
+from django.dispatch import receiver
+from cars.models import Car
+
+
+@receiver(pre_save, sender=Car)
+def car_pre_save(sender, instance, **kargs):
+    print('### PRE SAVE ###')
+
+
+@receiver(post_save, sender=Car)
+def car_post_save(sender, instance, **kargs):
+    print('### POST SAVE ###')
+
+
+@receiver(pre_delete, sender=Car)
+def car_pre_delete(sender, instance, **kargs):
+    print('### PRE DELETE ###')
+
+
+@receiver(post_delete, sender=Car)
+def car_post_delete(sender, instance, **kargs):
+    print('### POST DELETE ###')
