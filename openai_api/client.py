64ed63dadb34f5f3eb6b3246c290a04d96959fa5
Francisco <franciscojdsjr@gmail.com>
2023-11-15 10:04:41 -0300

    att

--- openai_api/client.py
+++ openai_api/client.py
@@ -5,7 +5,7 @@ def get_car_ai_bio(model, brand, year):
     prompt = f'''
     Me mostre uma descrição de venda para o carro {brand} {model} {year} em apenas 250 caracteres. Fale coisas especificas desse modelo de carro
     '''
-    openai.api_key = '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'
+    openai.api_key = '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'
     response = openai.Completion.create(
         model='text-davinci-003',
         prompt=prompt,
